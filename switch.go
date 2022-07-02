88cce150cfdd4126396d3047b3162d787bc04ad7
artizan <longdongzh@gmail.com>
2022-07-02 09:28:28 +0800

    switch

---
+++ switch.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Println("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday: // use commas to separate multiple expressions in the same case statement
+		fmt.Println("It's the weekend")
+	default: // optional
+		fmt.Println("It's a weekday")
+	}
+
+	// alternate way to express if/else logic
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// a type switch compares types instead of values
+	whatAmI := func(i any) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm a int ")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
